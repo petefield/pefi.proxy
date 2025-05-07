b8b813474c216f4d6aa181080d8a4363d79a717a
Pete Field <peter.field@newday.co.uk>
2025-05-07 11:32:56 +0100

    Add workflow

--- src/Program.cs
+++ src/Program.cs
@@ -1,13 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
-
 builder.Services.AddReverseProxy()
-    .ConfigureHttpClient((c, h) => {
-        h.AllowAutoRedirect = true;
-         
-    })
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 var app = builder.Build();
-
 app.MapReverseProxy();
-
 app.Run();
