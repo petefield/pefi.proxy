810519c86fef05afe73fc9a78d1b6fae6a8a268b
Pete Field <pete.field@gmail.com>
2025-05-12 23:17:16 +0100

    Combine Configs

--- src/Program.cs
+++ src/Program.cs
@@ -1,29 +1,44 @@
 using MongoDB.Bson;
 using pefi.Rabbit;
+using PeFi.Proxy;
 using PeFi.Proxy.Persistance;
 using Yarp.ReverseProxy.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<IDataStore, MongoDatastore>();
 builder.Services.AddHostedService<ProxyConfig>();
-builder.Services.AddControllers();
-builder.Services.AddReverseProxy().LoadFromMemory([], []);
+builder.Services.AddSwaggerGen();
+builder.Services.AddReverseProxy()
+    .LoadFromMemory([], [])
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
 builder.Services.AddSingleton<IMessageBroker>(sp => new MessageBroker("192.168.0.5", "username", "password"));
 
 var app = builder.Build();
 
-app.MapReverseProxy();
+app.MapGet("/config", (InMemoryConfigProvider memoryConfigProvider, IProxyConfigProvider  appSettingsConfigProvider ) => {
+    var appSettingsConfig = appSettingsConfigProvider.GetConfig();
+    var memoryConfig = appSettingsConfigProvider.GetConfig();
+
+    return new{
+        Routes =  appSettingsConfig.Routes.Concat(memoryConfig.Routes),
+        Clusters = appSettingsConfig.Clusters.Concat(memoryConfig.Clusters),
+    };
+}).WithName("Get Current Config")
+.WithOpenApi();
+
+app.UseStaticFiles();
 app.UseSwagger();
 app.UseSwaggerUI();
-app.MapGet("/config", (InMemoryConfigProvider configProvider) => {
-    var proxyConfig = configProvider.GetConfig();
-    return proxyConfig;
-})
-.WithName("Get Current Config")
-.WithOpenApi();
+app.UseRouting();
+
+app.MapReverseProxy();
+
+
+
+
 
 app.Run();
 
--- src/ProxyConfigUpdater.cs
+++ src/ProxyConfigUpdater.cs
@@ -1,10 +1,11 @@
 ﻿using pefi.Rabbit;
-using PeFi.Proxy;
 using PeFi.Proxy.Persistance;
 using Yarp.ReverseProxy.Configuration;
 
-public class ProxyConfig(ILogger<ProxyConfig> logger, 
-    IMessageBroker messageBroker, 
+namespace PeFi.Proxy;
+
+public class ProxyConfig(ILogger<ProxyConfig> logger,
+    IMessageBroker messageBroker,
     InMemoryConfigProvider configProvider,
     IDataStore dataStore) : BackgroundService
 {
@@ -13,8 +14,12 @@ public class ProxyConfig(ILogger<ProxyConfig> logger,
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         logger.LogInformation("Proxy Config Updater is listenting for new services.");
+        
         _topic = await messageBroker.CreateTopic("Events");
-        await _topic.Subscribe("#", async (key, message) => await UpdateConfig());
+
+        await _topic.Subscribe("#", async (key, message) 
+            => await UpdateConfig());
+
         await UpdateConfig();
     }
 
@@ -25,16 +30,16 @@ public class ProxyConfig(ILogger<ProxyConfig> logger,
         var routes = allServices
             .Select(serviceDescription => serviceDescription.ToRouteConfig())
             .Where(route => route != null)
-            .Select(x=>x!)
+            .Select(x => x!)
             .ToList();
 
         var clusters = allServices
             .Select(serviceDescription => serviceDescription.ToClusterConfig())
             .Where(cluster => cluster != null)
-            .Select(x=>x!)
+            .Select(x => x!)
             .ToList();
 
-        if (routes != null  && clusters != null)
+        if (routes != null && clusters != null)
             configProvider.Update(routes, clusters);
-    }   
+    }
 }
